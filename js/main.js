97061adba3ca1b0ac7c0c903e11477b0f16c9c6f
Subik Shrestha <subsatch@gmail.com>
2022-03-27 17:24:58 +0545

    home page design started, Navbar and Carousel added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+// Slider in home page
+$("#header_carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
 const hotels = document.querySelectorAll(".hotel_box");
 const tl = new gsap.timeline();
 tl.from(hotels, {
@@ -5,7 +24,7 @@ tl.from(hotels, {
   autoAlpha: 0,
   duration: 1,
   ease: Power3.easeOut,
-  stagger: { each: 0.1, from: "start" },
+  stagger: { each: 0.2, from: "start" },
 });
 
 tl.to(
