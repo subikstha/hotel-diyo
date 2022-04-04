0a04d7364561f85f41d111cf57c9a40e17f50740
Subik Shrestha <subsatch@gmail.com>
2022-04-04 14:46:25 +0545

    home page design in progress, gsap sticky text added, testimonial section completed

--- js/main.js
+++ js/main.js
@@ -17,6 +17,25 @@ $("#header_carousel").owlCarousel({
   },
 });
 
+$("#testimonial_carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: ['<img src="images/left.svg" />', '<img src="images/right.svg" />'],
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
 const hotels = document.querySelectorAll(".hotel_box");
 const tl = new gsap.timeline();
 tl.from(hotels, {
@@ -34,5 +53,43 @@ tl.to(
     duration: 0.5,
     ease: Power3.easeOut,
   },
-  "<0.75"
+  "<0.25"
 );
+
+tl.to(
+  ".select_dest-line",
+  {
+    width: "100%",
+  },
+  "="
+);
+
+const titles = ["Our Story", "Our Rooms", "Our Services"];
+const section = document.querySelectorAll(".section");
+// Pin the section title using gsap
+ScrollTrigger.create({
+  trigger: ".sections_container",
+  pin: ".sections_container-tooltip",
+  // markers: true,
+  start: "top 100px",
+});
+
+// Change the text when we enter the various sections
+section.forEach((section, index) => {
+  ScrollTrigger.create({
+    trigger: section,
+    // markers: true,
+    start: "top+=160 center",
+    end: "bottom center-=85",
+    onEnter: () => {
+      gsap.set(".sections_container-tooltip", {
+        innerText: titles[index],
+      });
+    },
+    onEnterBack: () => {
+      gsap.set(".sections_container-tooltip", {
+        innerText: titles[index],
+      });
+    },
+  });
+});
